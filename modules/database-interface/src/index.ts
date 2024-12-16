9e5f149d63dfbad7d78c32572ec6d676b31c1b12
wagseye <wagseye@gmail.com>
2024-12-16 15:44:23 -0800

    database-interface: Adding a new database-interface module
    Adding a module that only contains the interfaces for trusted/
    (implicitly untrusted) database interfaces. These interfaces are
    only to help out with Typescript during the development process,
    and should thus be installed only for development:
        npm install --save-dev database-interface

---
+++ modules/database-interface/src/index.ts
@@ -0,0 +1,29 @@
+export type DatabasePrimitive = string | number | Date;
+export type QueryKeys = "type" | "table" | "where" | "order" | "limit";
+
+export type Query = {
+  [key in QueryKeys]: any;
+};
+
+export type SqlKeys = "rows" | "error" | "loading";
+export type SqlResponse = {
+  [key in SqlKeys]: any;
+};
+export type QueryResponse = {
+  [index: string]: any;
+}[];
+
+export default interface DatabaseInterface {
+  query(query: object): Promise<QueryResponse>;
+}
+
+export interface TrustedDatabaseInterface extends DatabaseInterface {
+  runSql(
+    sql: string,
+    args: DatabasePrimitive | DatabasePrimitive[]
+  ): SqlResponse;
+  runQuery(
+    sql: string,
+    args: DatabasePrimitive | DatabasePrimitive[]
+  ): QueryResponse;
+}
