d85d128f0d9ffe23f4e8fe284d06b44c6282040e
wagseye <wagseye@gmail.com>
2024-12-20 04:40:53 -0800

    json-schema: Moved JSON validator from JTD to JSON schema
    In order to get unions working (to support reference fields) I had
    to rewrite the schema definitions using the JSON schema format.

--- modules/json-schema/src/validation.ts
+++ modules/json-schema/src/validation.ts
@@ -1,50 +1,102 @@
-import Ajv from "ajv/dist/jtd";
-import { JTDSchemaType } from "ajv/dist/jtd";
+import Ajv, { JSONSchemaType } from "ajv";
+import {
+  OrgSchema,
+  TableSchema,
+  FieldSchema,
+  ReferenceFieldSchema,
+} from "./schema";
 
-class FieldSchema {
-  name: string;
-  type: string;
-}
-
-interface TableSchema {
-  name: string;
-  namespace: string | undefined;
-  fields: FieldSchema[];
-}
-
-interface OrgSchema {
-  tables: TableSchema[];
-}
-
-const schemaTypeDefinition = {
-  definitions: {
-    fieldSchema: {
-      properties: {
-        name: { type: "string" },
-        type: { type: "string" },
-      },
-    },
-    tableSchema: {
-      properties: {
-        name: { type: "string" },
-        fields: {
-          elements: { ref: "fieldSchema" },
-        },
-      },
-      optionalProperties: {
-        namespace: { type: "string" },
-      },
-    },
-  },
+const orgSchema: JSONSchemaType<OrgSchema> = {
+  $id: "orgSchema",
+  type: "object",
+  additionalProperties: false,
   properties: {
     tables: {
-      elements: { ref: "tableSchema" },
+      type: "array",
+      items: { type: "object" } as JSONSchemaType<TableSchema>,
     },
   },
+  required: ["tables"],
+};
+
+const tableSchema: JSONSchemaType<TableSchema> = {
+  $id: "tableSchema",
+  type: "object",
+  additionalProperties: false,
+  properties: {
+    name: {
+      type: "string",
+    },
+    namespace: {
+      type: "string",
+      nullable: true,
+    },
+    fields: {
+      type: "array",
+      items: { type: "object" } as JSONSchemaType<
+        FieldSchema | ReferenceFieldSchema
+      >,
+    },
+  },
+  required: ["name", "fields"],
+};
+
+const fieldSchema: JSONSchemaType<FieldSchema> = {
+  $id: "fieldSchema",
+  type: "object",
+  additionalProperties: false,
+  properties: {
+    name: { type: "string" },
+    db_name: { type: "string" },
+    type: {
+      type: "string",
+      enum: ["id", "string", "number", "integer", "date", "datetime"],
+    },
+    required: { type: "boolean" },
+  },
+  required: ["name", "type"],
+};
+
+const referenceFieldSchema: JSONSchemaType<ReferenceFieldSchema> = {
+  $id: "referenceFieldSchema",
+  type: "object",
+  additionalProperties: false,
+  properties: {
+    name: { type: "string" },
+    db_name: { type: "string" },
+    type: {
+      type: "string",
+      enum: ["reference"],
+    },
+    foreignObject: { type: "string" },
+    foreignLookupField: { type: "string" },
+    foreignRelationName: { type: "string" },
+    required: { type: "boolean" },
+  },
+  required: [
+    "name",
+    "type",
+    "foreignObject",
+    "foreignLookupField",
+    "foreignRelationName",
+  ],
 };
 
 export function validateSchema(value: object) {
-  const ajv = new Ajv();
-  const validate = ajv.compile(schemaTypeDefinition);
-  return validate(value);
+  console.log("Validating JSON: " + JSON.stringify(value));
+  const ajv = new Ajv({ verbose: true });
+  const validate = ajv
+    .addSchema(fieldSchema)
+    .addSchema(referenceFieldSchema)
+    .addSchema(tableSchema)
+    .compile(orgSchema);
+
+  console.log("Compilation succeeded");
+
+  if (validate(value)) {
+    return true;
+  } else {
+    console.log("Validation errors: " + ajv.errorsText(validate.errors));
+    return false;
+  }
 }
