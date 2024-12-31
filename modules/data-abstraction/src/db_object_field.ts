34b822a3a8a516bb9d780f675ac305f5c8e0624d
wagseye <wagseye@gmail.com>
2024-12-31 10:42:38 -0800

    data-abstraction: Adding source files in modules directory
    I didn't realize this module had not yet been checked in. Adding
    the files to source control.

---
+++ modules/data-abstraction/src/db_object_field.ts
@@ -0,0 +1,219 @@
+"use strict";
+
+import { ConditionalExpression } from "./query.js";
+import { DbObjectMapper } from "./db_decorators.js";
+import DbObject from "./db_object.js";
+import Id from "./id.js";
+
+export type FieldType = string | number | Id | Date;
+
+export default abstract class DbObjectField {
+  #fieldName: string;
+
+  public get fieldName() {
+    return this.#fieldName;
+  }
+
+  public constructor(fieldName: string) {
+    if (!fieldName) throw new Error("A field name must be provided");
+    this.#fieldName = fieldName;
+  }
+
+  public abstract coerceType(value: FieldType): FieldType;
+
+  public equals(value: any): ConditionalExpression {
+    return new ConditionalExpression(this.fieldName, "=", value);
+  }
+
+  public notEquals(value: any): ConditionalExpression {
+    return new ConditionalExpression(this.fieldName, "<>", value);
+  }
+
+  public greaterThan(value: any): ConditionalExpression {
+    return new ConditionalExpression(this.fieldName, ">", value);
+  }
+
+  public greaterOrEqualThan(value: any): ConditionalExpression {
+    return new ConditionalExpression(this.fieldName, ">=", value);
+  }
+
+  public lessThan(value: any): ConditionalExpression {
+    return new ConditionalExpression(this.fieldName, "<", value);
+  }
+
+  public lessOrEqualThan(value: any): ConditionalExpression {
+    return new ConditionalExpression(this.fieldName, "<=", value);
+  }
+
+  public toString() {
+    console.log("In DbObjectField.toString(): " + this.#fieldName);
+    return this.#fieldName;
+  }
+}
+
+export class DbObjectIdField extends DbObjectField {
+  public constructor(fieldName: string) {
+    super(fieldName);
+  }
+
+  public coerceType(value: FieldType): Id {
+    if (value instanceof Id) return value;
+    if (typeof value === "string") return new Id(value);
+    throw new Error("Value of an id field must be an Id or string");
+  }
+}
+
+export class DbObjectStringField extends DbObjectField {
+  public constructor(fieldName: string) {
+    super(fieldName);
+  }
+
+  public coerceType(value: FieldType): string {
+    if (typeof value === "string") return value;
+    if (value instanceof Id) return value.toString();
+    throw new Error("Value of an string must be a string");
+  }
+}
+
+export class DbObjectNumberField extends DbObjectField {
+  #decimalPlaces;
+  public constructor(fieldName: string, decimalPlaces: number = undefined) {
+    super(fieldName);
+    this.#decimalPlaces = decimalPlaces;
+  }
+  public coerceType(value: FieldType): number {
+    if (typeof value === "number") return value;
+    try {
+      if (typeof value === "string") {
+        if (this.#decimalPlaces) {
+          // If we need to round, I think the best way for our purposes is to go from string => num => string => num.
+          // Another way is to multiply by 10^numdigits, round and divide back, but then you run into danger of overflow
+          // if you have a lot of decimal places you want to represent
+          const strRep = Number.parseFloat(value).toFixed(this.#decimalPlaces);
+          return Number.parseFloat(strRep);
+        }
+        return Number.parseFloat(value);
+      }
+    } catch (err: unknown) {
+      throw new Error("Invalid number format");
+    }
+    throw new Error("Value of an number field must be a number");
+  }
+}
+
+// This could extend DbObjectNumberField, but we're never going to need it for polymorphism, it
+// doesn't simplify the code, and it could throw off our unit tests when type checking
+export class DbObjectIntegerField extends DbObjectField {
+  public constructor(fieldName: string) {
+    super(fieldName);
+  }
+  public coerceType(value: FieldType): number {
+    if (typeof value === "number") return Math.floor(value);
+    try {
+      if (typeof value === "string") return Number.parseInt(value);
+    } catch (err: unknown) {
+      throw new Error("Invalid integer format");
+    }
+    throw new Error("Value of an integer field must be a number");
+  }
+}
+
+export class DbObjectDateField extends DbObjectField {
+  public constructor(fieldName: string) {
+    super(fieldName);
+  }
+  public coerceType(value: FieldType): Date {
+    if (value instanceof Date) return value;
+    if (typeof value === "number") {
+      // TODO: add additional checks here?
+      return new Date(value);
+    }
+    try {
+      if (typeof value === "string") return new Date(value);
+    } catch (err: unknown) {
+      throw new Error("Invalid date format");
+    }
+    throw new Error("Value can not be stored in a date field");
+  }
+}
+
+export class DbObjectDateTimeField extends DbObjectField {
+  public constructor(fieldName: string) {
+    super(fieldName);
+  }
+  public coerceType(value: FieldType): Date {
+    if (value instanceof Date) return value;
+    if (typeof value === "number") {
+      // TODO: add additional checks here?
+      return new Date(value);
+    }
+    try {
+      if (typeof value === "string") return new Date(value);
+    } catch (err: unknown) {
+      throw new Error("Invalid date format");
+    }
+    throw new Error("Value can not be stored in a datetime field");
+  }
+}
+
+export class DbObjectReferenceField extends DbObjectIdField {
+  #proto: Object | undefined;
+  #proto_name: string;
+  #relationName: string;
+  #foreignFieldName: string;
+  #foreignRelationName: string | undefined;
+
+  public constructor(
+    fieldName: string,
+    proto_name: string,
+    relationName: string,
+    foreignRelation: string | undefined = undefined,
+    foreignField: string = "id"
+  ) {
+    if (!proto_name) {
+      throw new Error(
+        "Object prototype name must be provided to create a DbObjectReferenceField object"
+      );
+    }
+    if (!foreignField) {
+      throw new Error(
+        "Reference field must be provided to create a DbObjectReferenceField object"
+      );
+    }
+    super(fieldName);
+    this.#proto_name = proto_name;
+    this.#relationName = relationName;
+    this.#foreignFieldName = foreignField;
+    this.#foreignRelationName = foreignRelation;
+  }
+
+  public get referenceType(): Object {
+    if (!this.#proto) {
+      this.#proto = DbObjectMapper.getObject(this.#proto_name);
+      if (!this.#proto)
+        throw new Error(
+          `Could not resolve class with name ${this.#proto_name}`
+        );
+    }
+    return this.#proto;
+  }
+
+  public get foreignField(): string {
+    return this.#foreignFieldName;
+  }
+
+  public get relationName(): string {
+    return this.#relationName;
+  }
+
+  public get foreignRelationName(): string | undefined {
+    return this.#foreignRelationName;
+  }
+
+  // NB: How do we store the actual reference to the related object?
+
+  // protected coerceType(value: unknown) : unknown {
+  //   if (value instanceof DbObject) return value;
+  //   throw new Error("Value of an id field must be an Id or string");
+  // }
+}
---
+++ modules/data-abstraction/src/id.ts
@@ -0,0 +1,55 @@
+"use strict";
+
+const OBJECT_PREFIX_WIDTH = 2;
+const API_VERSION_WIDTH = 1;
+const ORGANIZATION_WIDTH = 4;
+const UNIQUE_ID_WIDTH = 7;
+
+/*
+The Id format is as follows:
+  2 chars (1.3k) are object prefix
+    Numeric starting characters are built-in objects
+    Letter startings characters are custom objects
+  1 chars (36) is API version
+  4 chars (1.7M) are org id
+  7 chars (78B) are uid
+*/
+
+export default class Id extends String {
+  #id: string;
+
+  public static isId(val: string) {
+    if (!(typeof val === "string")) return false;
+    return new RegExp("[A-Za-z0-9]{15}").test(val);
+  }
+
+  constructor(id: string) {
+    super();
+    if (!Id.isId(id)) throw new Error(`Invalid id: ${id}`);
+
+    this.#id = id;
+  }
+
+  public toString(): string {
+    return this.#id;
+  }
+
+  public getOrganizationId(): string {
+    return this.#id.substring(0, OBJECT_PREFIX_WIDTH);
+  }
+
+  public getApiVersion(): string {
+    const START = OBJECT_PREFIX_WIDTH;
+    return this.#id.substring(START, START + API_VERSION_WIDTH);
+  }
+
+  public getOrganization(): string {
+    const START = OBJECT_PREFIX_WIDTH + API_VERSION_WIDTH;
+    return this.#id.substring(START, START + ORGANIZATION_WIDTH);
+  }
+
+  public getUniqueId(): string {
+    const START = OBJECT_PREFIX_WIDTH + API_VERSION_WIDTH + ORGANIZATION_WIDTH;
+    return this.#id.substring(START, START + UNIQUE_ID_WIDTH);
+  }
+}
