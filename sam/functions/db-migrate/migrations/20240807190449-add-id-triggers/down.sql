a5bccff1587e28df48b318bdc6807c9b4e4f05d0
wagseye <wagseye@gmail.com>
2025-02-02 09:37:52 -0800

    db-migrate: Deleting child sequence records in down script
    For the add-id-triggers migration, we insert records into the objects table which automatically creates corresponding object_sequence records, but in the down script we were not deleting these child records. This has been fixed so we are doing the necessary cleanup.

--- sam/functions/db-migrate/migrations/20240807190449-add-id-triggers/down.sql
+++ sam/functions/db-migrate/migrations/20240807190449-add-id-triggers/down.sql
@@ -1,11 +1,15 @@
 DROP TRIGGER populate_public_object_fields_id ON object_fields;
-DELETE FROM objects WHERE table_schema='public' AND table_name='object_field';
+DELETE FROM object_sequences WHERE organization_key='0001' AND object_prefix='016';
+DELETE FROM objects WHERE table_schema='public' AND table_name='object_fields';
 
 DROP TRIGGER populate_public_users_id ON users;
-DELETE FROM objects WHERE table_schema='public' AND table_name='user';
+DELETE FROM object_sequences WHERE organization_key='0001' AND object_prefix='002';
+DELETE FROM objects WHERE table_schema='public' AND table_name='users';
 
 DROP TRIGGER populate_public_organizations_id ON organizations;
-DELETE FROM objects WHERE table_schema='public' AND table_name='organization';
+DELETE FROM object_sequences WHERE organization_key='0001' AND object_prefix='001';
+DELETE FROM objects WHERE table_schema='public' AND table_name='organizations';
 
 DROP TRIGGER populate_public_objects_id ON objects;
-DELETE FROM objects WHERE table_schema='public' AND table_name='object';
+DELETE FROM object_sequences WHERE organization_key='0001' AND object_prefix='015';
+DELETE FROM objects WHERE table_schema='public' AND table_name='objects';
